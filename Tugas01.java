45c84a67ee6b14231a67d624fc2efe75eb47f03f
Ahmad Sofyan Badawi <sopyanbadawi@gmail.com>
2024-09-04 10:37:29 +0700

    Biodata Ahmad Sofyan Badawi

---
+++ Tugas01.java
@@ -0,0 +1,15 @@
+public class Tugas01 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama \t\t: Ahmad Sofyan Badawi");
+        System.out.println("NIM  \t\t: 24410702003");
+        System.out.println("Kelas \t\t: 1B");
+        System.out.println("No. Absen\t: 04");
+        System.out.println("Prodi \t\t: Teknik Informatika");
+        System.out.println("Alamat \t\t: Jl. Kembang Kerta No.25A, Jatimulyo");
+        System.out.println("No. HP \t\t: 085960622069");
+        System.out.println("Hobi \t\t: Basket");
+    }
+}
